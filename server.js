c3d6af1e453624569350e3460d06da04a92488a8
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-22 18:14:45 +1200

    working on render

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: true },
     host : process.env.DATABASE_HOST,
     port : 5432,
     user : process.env.DATABASE_USER,
